1bb35159ec3de0abfcbb65aa76db1c85c35822bf
Foday  Christopher Koroma <koromafoday840@gmail.com>
2026-03-04 12:02:43 +0000

    Add manual dictionary logic for student scores

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-def validate_user_data(age_text):
-    """Calculates age for next year from input string."""
-    try:
-        current_age = int(age_text)
-        next_year_age = current_age + 1
-        return f"Next year you will be {next_year_age}."
-    except ValueError:
-        return "Please enter a valid number!"
-
-@app.route('/')
-def home():
-    # This creates a simple web form with a box and a button
-    return """
-        <h1>Age Calculator</h1>
-        <form action="/calculate" method="POST">
-            <input type="text" name="user_age" placeholder="Enter your age">
-            <button type="submit">Calculate</button>
-        </form>
-    """
-
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    # This gets the data from the box and runs your function
-    age_input = request.form.get('user_age')
-    result = validate_user_data(age_input)
-    return f"<h1>Result</h1><p>{result}</p><a href='/'>Go Back</a>"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=81)
+"""
+Project: Student Database Logic
+Author: Foday C. Koroma 
+Description: This script shows manual iteration over a dictionary to find maximum value without using built-in functions like max().
+"""
+student_db = {
+  "Foday": 98,
+  "John": 83,
+  "Mary": 71,
+  "Ali": 97,
+  "Sarah":45
+}
+highest_name = ""
+highest_score = -1
+for name in student_db:
+  current_score = student_db[name]
+  if current_score > highest_score:
+    highest_score = current_score
+    highest_name = name
+print(f"Highest scorer: {highest_name} {highest_score}") 
+student_db["Sarah"] = 88
+del student_db["Mary"]
+print("Final database:", student_db)
