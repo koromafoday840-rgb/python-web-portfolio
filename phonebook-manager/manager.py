581cd0c2c8ea9eba5f79585dc0a231f7d4f42d9f
Foday  Christopher Koroma <koromafoday840@gmail.com>
2026-03-16 10:03:41 +0000

    update phonebook

--- phonebook-manager/manager.py
+++ phonebook-manager/manager.py
@@ -1,31 +1,34 @@
 phonebook = {}
-print("___Phonebook manager system___")
+print("___Interactive Phonebook Manager___")
 while True:
-    print("\nAction: [1] Add/update [2] Delete [3] Search [4] View All [5] Exit")
-    choice = input("select an option: ")
-    if choice == "1":
-        name = input("enter contact name: ").strip()
-        number = input("enter contact number: ").strip()
-        phonebook[name] = number
-        print(f"success: {name} saved")
-    elif choice == "2":
-        name = input("enter name to delete: ").strip()
-        if name in phonebook:
-            del phonebook[name]
-            print(f"success: {name} removed")
-            print("error: contact not found")
-    elif choice == "3":
-        name = input("search name: ").strip()
-        result = phonebook.get(name, "contact not found")
-        print(f"result: {result}")
-    elif choice == "4":
-        if not phonebook:
-            print("phonebook is currently empty")
-        else:
-            for name, number in phonebook.items():
-                print(f"- {name}: {number}")
-    elif choice == "5":
-        print("exiting system. goodbye!")
-        break
-else:
-    print("invalid choice. please try again")
+  print()
+  print("Action: [1] add/update [2] delete [3] search [4] view all [5] exit")
+  choice = input("select an option")
+  if choice == "1":
+    name = input("enter contact name").strip()
+    number = input("enter the contact number").strip()
+    phonebook[name] = number
+    print(f"success: {name} saved")
+  elif choice == "2":
+    name = input("enter name to delete").strip()
+    if name in phonebook:
+      del phonebook[name]
+    else:
+      print("error. contact not found")
+  elif choice == "3":
+    name = input("search name").strip()
+    result = phonebook.get(name, 'contact not found')
+    print(f"result: {result}")
+  elif choice == "4":
+    if not phonebook:
+      print("phonebook is currently empty")
+    else:
+      for name, number in phonebook.items():
+        print(f"- {name} {number}")
+  elif choice == "5":
+    print("exiting system. goodbye!")
+    break
+  print("invalid choice. please try again")  
+    
+    
+  
